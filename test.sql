b75a28bc51798548d73619b36ffb53446cff1007
madalenamelo <72252309+madalenamelo@users.noreply.github.com>
2020-10-19 11:40:30 +0100

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,9 +1,6 @@
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-syntax error at or near "ON"
 SET QUOTED_IDENTIFIER ON 
-syntax error at or near "ON"
 SET ANSI_NULLS ON 
-syntax error at or near "IF"
 IF EXISTS 
 (
   SELECT * 
